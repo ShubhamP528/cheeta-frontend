46609d271a2d7e0cacee293a9138e32b053fb980
Shubham Prajapati <shubham528prajapati@gmail.com>
2024-12-29 15:25:01 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 import { retrieveAuth } from "./features/auth";
+import { NODE_API_ENDPOINT } from "./utils/utils";
 
 function App() {
   const dispatch = useDispatch();
@@ -16,6 +17,29 @@ function App() {
   useEffect(() => {
     dispatch(retrieveAuth());
   }, [dispatch]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${NODE_API_ENDPOINT}/chat/hii`, {
+          method: "GET",
+        });
+        console.log(response);
+        console.log("Data fetched successfully");
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Fetch data initially
+    fetchData();
+
+    // Fetch data every 10 minutes
+    const interval = setInterval(fetchData, 1 * 60 * 1000);
+
+    // Cleanup function to clear interval
+    return () => clearInterval(interval);
+  }, []); // Empty dependency array ensures this effect runs only once
 
   // Don't render Footer on the chat page
   const shouldShowFooter = !location.pathname.startsWith("/chat");
